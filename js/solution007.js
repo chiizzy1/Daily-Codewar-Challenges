1d0f77d52dc4b64dc3ed319f0ca1f46953ff479a
Chinwuba Israel <chiizzy12a@gmail.com>
2022-05-14 12:58:14 +0100

    complete solution 007 first try

---
+++ js/solution007.js
@@ -0,0 +1,13 @@
+/*
+var humanYearsCatYearsDogYears = function(humanYears) {
+    // Your code here!
+    if(humanYears=== 1){
+      return [1,15,15]
+    }else if(humanYears === 2){
+      return [2,24,24]
+    }else if(humanYears > 2){
+        let n = humanYears - 2
+        return [humanYears, 24+(n*4),24+(n*5)]
+    }
+  }
+*/
