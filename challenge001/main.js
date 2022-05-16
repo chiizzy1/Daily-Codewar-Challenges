ee4ece54e9d842835a5ded8bc61e0c50834ba267
Chinwuba Israel <chiizzy12a@gmail.com>
2022-05-16 08:47:49 +0100

    complete challenge001

---
+++ challenge001/main.js
@@ -0,0 +1,16 @@
+/*  *****   
+Challenge: Name Shuffler
+
+Instructions:Write a function that returns a string in which firstname is swapped with last name.
+
+nameShuffler('john McClane'); => "McClane john"
+              
+
+                                       MY Solution
+
+                                       :)
+                                       CodeWars Solution
+   function nameSuffle(str){
+        return str.split(' ').reverse().join(' ')
+    }
+*/
